63fee507827d2407e1394a1899ef2579fcc8c2ac
Abdullah1428 <abdullahcse1428@gmail.com>
2021-08-25 14:58:15 +0200

    finished intro section and added animations

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,41 @@
+import { useEffect, useRef } from 'react'
+
 import './intro.scss'
 
+import { init } from 'ityped'
+
 export default function Intro() {
-  return <div className='intro' id='intro'></div>
+  const textRef = useRef()
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: false,
+      backDelay: 1500,
+      backSpeed: 60,
+      showCursor: true,
+      strings: ['Developer', 'Engineer']
+    })
+  }, [])
+
+  return (
+    <div className='intro' id='intro'>
+      <div className='left'>
+        <div className='imgContainer'>
+          <img src='assets/me.jpeg' alt='profile picture' />
+        </div>
+      </div>
+      <div className='right'>
+        <div className='wrapper'>
+          <h2>Hi there, I'm</h2>
+          <h1>Abdullah</h1>
+          <h3>
+            A Software <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href='#portfolio'>
+          <img src='assets/down.png' alt='down icon' />
+        </a>
+      </div>
+    </div>
+  )
 }
